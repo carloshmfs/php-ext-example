5b46df90b12ff28aa01abe224fc5cc8d113c1e4a
carloshmfs <carloshmfs.dev@pm.me>
2024-08-26 21:17:32 -0300

    init commit

---
+++ Main.cpp
@@ -0,0 +1,37 @@
+#include <phpcpp.h>
+
+#include <iostream>
+
+Php::Value myFirstFunction()
+{
+    Php::out << "Hello World" << std::endl;
+
+    return true;
+}
+
+/**
+ *  tell the compiler that the get_module is a pure C function
+ */
+extern "C" {
+
+    /**
+     *  Function that is called by PHP right after the PHP process
+     *  has started, and that returns an address of an internal PHP
+     *  strucure with all the details and features of your extension
+     *
+     *  @return void*   a pointer to an address that is understood by PHP
+     */
+    PHPCPP_EXPORT void *get_module() 
+    {
+        // static(!) Php::Extension object that should stay in memory
+        // for the entire duration of the process (that's why it's static)
+        static Php::Extension extension("hello-world", "1.0");
+
+        // @todo    add your own functions, classes, namespaces to the extension
+        extension.add<myFirstFunction>("caralho");
+
+        // return the extension
+        return extension;
+    }
+}
+
